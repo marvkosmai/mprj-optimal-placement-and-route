338de5fb0f755a1874d323235850ec660557af05
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2022-11-22 17:36:10 +0100

    add grid and grid computation

---
+++ Assets/ComputedGridPoint.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputedGridPoint
+{
+    public Vector3 location;
+    public List<bool> coverage;
+}
--- Assets/OptimalPlacementAndRoute.cs
+++ Assets/OptimalPlacementAndRoute.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
 
 public class OptimalPlacementAndRoute : MonoBehaviour
@@ -22,27 +24,79 @@ public class OptimalPlacementAndRoute : MonoBehaviour
 
     [Space(10)]
 
+    // Grid
+    [Header("Grid")]
+    [Range(0.001f, 10.0f)]
+    public float gridSpace = 1.0f;
+    [Range(0.0f, 10.0f)]
+    public float expandBox = 0.0f;
+    [Range(0.0f, 10.0f)]
+    public float raiseBox = 0.0f;
+    public bool standingOnGround = true;
+    
+    public enum InteractiveMode { On, Off }
+    public InteractiveMode interactiveMode = InteractiveMode.On;
+
+    private List<Vector3> gridPoints = new List<Vector3>();
+    private Bounds originalBounds;
+    private Bounds bounds;
+    private Vector3 boxTopLeft;
+
+    [Space(10)]
+
+    // EA Pre Computation
+    [Header("Pre Computation")]
+    [Range(0.01f, 10.0f)]
+    public float scanningRange = 3.0f;
+    [Range(1.0f, 180.0f)]
+    public float maxScanningAngle = 60.0f;
+
+    private List<ComputedGridPoint> computedGridPoints = new List<ComputedGridPoint>();
+    private bool gridPointsComputed = false;
+
+    [Space(10)]
+
     // Gizmo
     [Header("Gizmos")]
     public bool showSamples = true;
+    public bool showGridBox = true;
+    public bool showGridPoints = true;
+    public bool showComputedGridPoint = false;
+    [Range(0, 2000)]
+    public int showComputedGridPointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         Init();
         GetSamples();
+        if (interactiveMode == InteractiveMode.Off)
+        {
+            GenerateGrid();
+            ComputeRasterPoints();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (interactiveMode == InteractiveMode.On)
+        {
+            GenerateGrid();
+            return;
+        }
+
+        if (!gridPointsComputed)
+        {
+            ComputeRasterPoints();
+        }
         
     }
 
     // Draws Gizmos
     private void OnDrawGizmos()
     {
-        if (showSamples)
+        if (showSamples && (!gridPointsComputed || !showComputedGridPoint))
         {
             Gizmos.color = Color.green;
             foreach (Vector3 p in samplePoints)
@@ -50,12 +104,47 @@ public class OptimalPlacementAndRoute : MonoBehaviour
                 Gizmos.DrawSphere(p, 0.1f);
             }
         }
+
+        if (showGridBox)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireCube(
+                bounds.center,
+                bounds.size
+            );
+        }
+
+        if (showGridPoints && (!gridPointsComputed || !showComputedGridPoint))
+        {
+            Gizmos.color = Color.blue;
+            foreach (Vector3 p in gridPoints)
+            {
+                Gizmos.DrawSphere(p, 0.1f);
+            }
+        }
+
+        if (gridPointsComputed && showComputedGridPoint && (showComputedGridPointIndex < computedGridPoints.Count))
+        {
+            ComputedGridPoint computedGridPoint = computedGridPoints[showComputedGridPointIndex];
+
+            for (int i = 0; i < nSamples; i++)
+            {
+                if (computedGridPoint.coverage[i]) Gizmos.color = Color.green;
+                else Gizmos.color = Color.red;
+
+                Gizmos.DrawSphere(samplePoints[i], 0.1f);
+            }
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(computedGridPoint.location, 0.2f);
+        }
     }
     // --- INIT
     void Init()
     {
         Random.InitState(System.Guid.NewGuid().GetHashCode());
 
+        // SAMPLES
         if (null == mesh)
         {
             mesh = GetComponent<MeshFilter>().mesh;
@@ -71,14 +160,154 @@ public class OptimalPlacementAndRoute : MonoBehaviour
                 collider = gameObject.AddComponent<MeshCollider>();
             }
         }
+
+        // GRID
+        if (true)
+        {
+            originalBounds = GetComponent<Renderer>().bounds;
+        }
     }
 
     // INIT ---
 
+    // --- EA LOCATION PRE COMPUTATION
+    void ComputeRasterPoints()
+    {
+        computedGridPoints.Clear();
+        Debug.Log("Computing grid points...");
+        foreach (Vector3 gridPoint in gridPoints)
+        {
+            NativeArray<RaycastHit> results = new NativeArray<RaycastHit>(nSamples, Allocator.TempJob);
+            NativeArray<RaycastCommand> commands = new NativeArray<RaycastCommand>(nSamples, Allocator.TempJob);
+
+            for (int i = 0; i < nSamples; i++)
+            {
+                Vector3 origin = samplePoints[i];
+                Vector3 direction = (gridPoint - origin).normalized;
+                commands[i] = new RaycastCommand(origin, direction);
+            }
+
+            JobHandle handle = RaycastCommand.ScheduleBatch(commands, results, nSamples, default(JobHandle));
+
+            handle.Complete();
+
+            List<bool> hits = new List<bool>();
+            for (int i = 0; i < nSamples; i++)
+            {
+                RaycastHit batchedHit = results[i];
+
+                Vector3 origin = samplePoints[i];
+                float length = Vector3.Distance(origin, gridPoint);
+
+                float angle = Vector3.Angle(origin, gridPoint);
+
+                // only when the ray does not hit a surface,
+                // is shorter than the given length
+                // and the angle is smaller than the given angle
+                // then the sample counts as beeing seen
+                hits.Add(
+                    batchedHit.collider == null 
+                    && length <= scanningRange 
+                    && angle <= maxScanningAngle
+                    );
+            }
+
+            ComputedGridPoint computedGridPoint = new ComputedGridPoint();
+            computedGridPoint.location = gridPoint;
+            computedGridPoint.coverage = hits;
+            computedGridPoints.Add(computedGridPoint);
+
+            results.Dispose();
+            commands.Dispose();
+        }
+        Debug.Log($"{computedGridPoints.Count} grid points computed");
+
+        gridPointsComputed = true;
+    }
+
+    // EA LOCATION PRE COMPUTATION ---
+
+    // --- GRID
     void GenerateGrid()
     {
+        gridPoints.Clear();
 
+        bounds = originalBounds;
+        bounds.Expand(expandBox);
+        if (standingOnGround)
+        {
+            bounds.center = bounds.center + new Vector3(0, expandBox / 2 + raiseBox, 0);
+        }
+        boxTopLeft = bounds.center + bounds.extents;
+
+        float xOffset = (bounds.size.x % gridSpace) / 2;
+        float yOffset = (bounds.size.y % gridSpace) / 2;
+        float zOffset = (bounds.size.z % gridSpace) / 2;
+
+        for (int x = 0; x < bounds.size.x / gridSpace; x++)
+        {
+            for (int y = 0; y < bounds.size.y / gridSpace; y++)
+            {
+                for (int z = 0; z < bounds.size.z / gridSpace; z++)
+                {
+                    Vector3 X = new Vector3(gridSpace * x + xOffset, 0, 0);
+                    Vector3 Y = new Vector3(0, gridSpace * y + yOffset, 0);
+                    Vector3 Z = new Vector3(0, 0, gridSpace * z + zOffset);
+
+                    Vector3 P = boxTopLeft - X - Y - Z;
+
+                    if (!IsInCollider(collider, P))
+                    {
+                        gridPoints.Add(P);
+                    }
+                }
+            }
+        }
     }
+    private bool IsInCollider(MeshCollider other, Vector3 point)
+    {
+        Vector3 from = (Vector3.up * 5000f);
+        Vector3 dir = (point - from).normalized;
+        float dist = Vector3.Distance(from, point);
+        //fwd      
+        int hit_count = CastTill(from, point, other);
+        //back
+        dir = (from - point).normalized;
+        hit_count += CastTill(point, point + (dir * dist), other);
+
+        if (hit_count % 2 == 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int CastTill(Vector3 from, Vector3 to, MeshCollider other)
+    {
+        int counter = 0;
+        Vector3 dir = (to - from).normalized;
+        float dist = Vector3.Distance(from, to);
+        bool Break = false;
+        while (!Break)
+        {
+            Break = true;
+            RaycastHit[] hit = Physics.RaycastAll(from, dir, dist);
+            for (int tt = 0; tt < hit.Length; tt++)
+            {
+                if (hit[tt].collider == other)
+                {
+                    counter++;
+                    from = hit[tt].point + dir.normalized * .001f;
+                    dist = Vector3.Distance(from, to);
+                    Break = false;
+                    break;
+                }
+            }
+        }
+        return counter;
+    }
+
+    // GRID ---
 
     // --- SAMPLES
     void GetSamples()
